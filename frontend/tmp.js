6c9ad0fadede9ec9a119adff8a5cf9a6f7f11457
Ewa Kadziolka <kalistaewa@gmail.com>
2022-12-13 11:11:46 +0100

    adds color to helptText when validation changes)

--- frontend/tmp.js
+++ frontend/tmp.js
@@ -1,11 +1,19 @@
 const form = document.querySelector("form");
-//const helpText = document.getElementsByClassName("form-text");
+const helpText =document.querySelectorAll("form-text");
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 const myToast = document.getElementById('toast');
 const toast = new bootstrap.Toast(myToast);
+const inputs = document.querySelectorAll("input")
 
 const elements = form.elements;
 console.log(elements);
+
+/* for (const input of inputs){
+    if(input.value == null){
+        helpText.classList.add("text-danger")
+    }
+} */
+
 
 for (const element of elements) { // for(String str: strings)
     const type = element.type;
@@ -17,24 +25,35 @@ for (const element of elements) { // for(String str: strings)
         element.min = `${yyyy}-${mm}-${dd}`;
     }
 
-   if (type != "submit") {
-        element.addEventListener("input", (event)=>{
-            //const validityState = input.validity;
-            if(element.validity.valid){
-                element.classList.remove("is-invalid")
-                element.classList.add("is-valid");
 
+   if (type != "submit") {
+        //element.classList.add("is-invalid");
+        element.addEventListener("input", (event)=>{
+            const name = element.id;
+            const helpText = document.getElementById(`${name}HelpBlock`)
+            
+            if(element.validity.valid){
+                element.classList.remove("is-invalid");
+                element.classList.add("is-valid");
+                helpText.classList.remove("text-danger");
+                helpText.classList.add("text-success");
             }
             else if( element.classList.contains('is-invalid') && element.validity.valid){
-                element.classList.remove("is-invalid")
+                element.classList.remove("is-invalid");
                 element.classList.add("is-valid");
-               
+                helpText.classList.remove("text-danger");
+                helpText.classList.add("text-success");
             }
             else{
                 element.classList.remove("is-valid");
                 element.classList.add("is-invalid");
+                helpText.classList.remove("text-succes");
+                helpText.classList.add("text-danger");
+
             }
+
         })
+        
     }
 }
 
@@ -44,12 +63,20 @@ const tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
       })
 })
 
+/* function changesValidations(){
+    const name = element.name;
+    const helpText = document.getElementById(`${name}HelpBlock`)
+    helpText.classList.remove("text-danger");
+    helpText.classList.add("text-success");
+} */
+
+//element.addEventListener("change", changesValidations);
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     form.reset();
     for (const element of elements){
         element.classList.remove("is-valid")
     }
-    console.log("implement toaster");
     toast.show();
 })
